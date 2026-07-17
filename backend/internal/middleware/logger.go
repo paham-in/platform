98f6a50e0f719e1512c0524a6181c10178f5fd76
Rizal Anggoro <gnoogler4@gmail.com>
2026-07-18 06:05:53 +0700

    feat: simplify request logging middleware by utilizing built-in logger

--- backend/internal/middleware/logger.go
+++ backend/internal/middleware/logger.go
@@ -1,25 +1,12 @@
 package middleware
 
 import (
-	"log"
-	"time"
-
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func RequestLogger() fiber.Handler {
-	return func(c *fiber.Ctx) error {
-		start := time.Now()
-		err := c.Next()
-		dur := time.Since(start)
-
-		log.Printf("[%s] %s %s %d %s",
-			c.IP(),
-			c.Method(),
-			c.Path(),
-			c.Response().StatusCode(),
-			dur,
-		)
-		return err
-	}
+	return logger.New(logger.Config{
+		Format: "[${ip}] ${method} ${path} ${status} ${latency}\n",
+	})
 }
