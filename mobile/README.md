d79c51631c5265c1de05cd50ecf337eeea01cc11
Rizal Anggoro <gnoogler4@gmail.com>
2026-09-03 12:10:27 +0700

    chore: remove mobile Flutter app, web-only focus

--- mobile/README.md
+++
@@ -1,3 +0,0 @@
-# pahamin_mobile
-
-A new Flutter project.
--- mobile/android/app/src/main/kotlin/com/pahamin/pahamin_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.pahamin.pahamin_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
